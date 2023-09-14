fbe5e06ecb85311fe15df6e3116b876a8ae10ed3
rohith4088 <74166452+rohith4088@users.noreply.github.com>
2023-09-14 14:28:26 +0530

    data_ingestion

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+# from terrainclassifier.logging import logger
+# logger.info("hello")
+from terrainclassifier.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from terrainclassifier.logging import logger
-logger.info("hello")
